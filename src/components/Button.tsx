a662ce719622370c63c69426f26bf628e06f7b51
percy <percy.u.lomas@gmail.com>
2023-10-27 00:50:10 -0500

    fix-button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-function Button(props: { children: React.ReactNode, onClick: () => void }): JSX.Element {
+function Button(props: { children: React.ReactNode}): JSX.Element {
   
     return (
         <button className='bg-rojo text-white font-[poppins] py-2 px-6 rounded md:ml-8 hover:bg-indigo-400 duration-500 '>
