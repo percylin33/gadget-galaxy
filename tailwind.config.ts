5fe70de34bb05099190a09a8e66c925429abbda4
percy <percy.u.lomas@gmail.com>
2023-10-27 00:46:39 -0500

    fix-colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,10 +14,11 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
-        'negroo': '#373234',
         'plomo': '#c8d7d2',
+        'nav': '#444',
         'gris': '#f4f2f0',
         'rojo': '#d84339',
+        'verde':'#c8d7d2'
       },
     },
   },
