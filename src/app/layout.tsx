51c05166f61d5778055b0e42cf592d401ab2d7d1
percy <percy.u.lomas@gmail.com>
2023-10-27 00:47:13 -0500

    modify-layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,13 +13,13 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       
       <body className={inter.className}>
-        <Navbar />
+        <Navbar  />
         {children}
         </body>
     </html>
