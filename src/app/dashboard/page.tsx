90016a5ac936b15e546056e38b6ab6786b03f676
percy <percy.u.lomas@gmail.com>
2023-10-27 00:48:23 -0500

    fix-dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Sidebar from "@/components/Sidebar"
+
+
+export default function dashboardPage() {
+    return (
+      <div className=' grid grid-cols-6 min-h-screen '>
+        
+        <Sidebar />
+      </div>
+    )
+  }
+  
