984e4ef6a3593d19657f8df41bf083c3fb215875
percy <percy.u.lomas@gmail.com>
2023-10-28 02:30:26 -0500

    fix-nav-styles

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,20 +1,53 @@
-import { IoAlbums, IoBarChart, IoBuild, IoExit, IoStorefront } from 'react-icons/io5';
+'use client'
+
+import {useState} from 'react'
+import { IoAlbums, IoBarChart, IoBuild, IoChevronBackCircle, IoStorefront, IoBook, IoPeople } from 'react-icons/io5';
+import Link from 'next/link';
 
 function Sidebar(){
-  
+       const [showMenu, setShowMenu] = useState(false);
     return (
-        <div className='bg-plomo h-full overflow-y-scroll '>
+    <>
+    <div className={`bg-plomo h-full fixed lg:static w-[80%] left-0  ${showMenu ? "bottom-0" : "-bottom-full" }`}>
             <div className="flex flex-col items-center justify-center p-8 gap-2">
-                <img src="https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-High-Quality-Image.png" alt="imagen" className="w-20 h-20 object-cover rounded-full" />
+                <img src="https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-High-Quality-Image.png" alt="imagen" 
+                className="w-20 h-20 object-cover rounded-full " />
             </div>
             <h1 className="text-xl font-bold text-center">Percy Valderrama</h1>
         
-            <p className="bg-rojo m-auto my-2  rounded-full py-1 px-3 text-center w-24">Pro level</p>
+            <p className="bg-rojo m-auto my-2  rounded-full py-1 px-3 text-center w-24 text-white">Pro level</p>
         {/* nav */}
-        <nav className='bg-teal-100 p-8 rounded-tr-[100px] h-[70vh]'>
+        <div className='bg-teal-100 p-6 rounded-tr-[100px] h-[70vh] flex flex-col justify-between'>
+        <nav>
+            <Link href='#' className='flex items-center gap-4 py-2 px-2 rounded-xl hover:bg-plomo/50 transition-colors'>
+                <IoStorefront />Store
+            </Link>
+            <Link href='#' className='flex items-center gap-4 py-2 px-2 rounded-xl hover:bg-plomo/50 transition-colors'>
+                <IoBarChart />Graphic
+            </Link>
+            <Link href='#' className='flex items-center gap-4 py-2 px-2 rounded-xl hover:bg-plomo/50 transition-colors'>
+                <IoAlbums />Sales
+            </Link>
+            <Link href='#' className='flex items-center gap-4 py-2 px-2 rounded-xl hover:bg-plomo/50 transition-colors'>
+                <IoBook />Forms
+            </Link>
+            <Link href='#' className='flex items-center gap-4 py-2 px-2 rounded-xl hover:bg-plomo/50 transition-colors'>
+                <IoPeople />Users
+            </Link>
 
         </nav>
+        <div className='bg-plomo/50 p-2 rounded-xl'>
+            <p className='text-center text-gray-800'>Having Problems ?</p>
+            <Link href='#'> <p className='text-sm text-center font-bold'>Contact Us</p></Link>
+
         </div>
+        </div>
+    </div>
+    {/* boton movil */}
+    <button onClick={()=> setShowMenu(!showMenu)} className='fixed right-4 bottom-4 text-2xl'>
+        <IoChevronBackCircle />
+    </button>
+    </>
     );
 }
 
