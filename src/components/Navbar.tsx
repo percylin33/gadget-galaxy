76c3da3db965d3ea7adf6df790b30bf470c87386
percy <percy.u.lomas@gmail.com>
2023-10-31 01:19:09 -0500

    styles-navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -29,7 +29,7 @@ function Navbar()  {
 
 
     return (
-        <div className={`shadow-md w-full sticky top-0 left-0 `}>
+        <div className={`shadow-md w-full sticky top-0 left-0 z-30`}>
             <div className='md:flex bg-nav items-center justify-between py-4 md:px-10 px-7'>
                 <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-gray-800'>
                     <Image src={logo} alt="imagen logo" className='w-14 h-14' />
@@ -40,7 +40,7 @@ function Navbar()  {
                 <div onClick={()=>setOpen(!open)} className='text-plomo text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
                     {open ? <IoCloseCircleSharp /> : <IoMenu />}
                 </div>
-                <ul className={`bg-nav md:flex md:items-center md:pb-0 pb-12 absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-[90px] opacity-100':'top-[-490px] md:opacity-100 opacity-0'} `}>
+                <ul className={`bg-nav md:flex md:items-center md:pb-0 pb-12 absolute md:static md:z-auto z-25 left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-[90px] opacity-100':'top-[-490px] md:opacity-100 opacity-0'} `}>
                     {linkNav.map((li) => (
                         <li key={li.name} className='md:ml-8 text-xl md:my-0 my-7' onClick={closeMenu}>
                             <Link href={li.link} className='text-gris hover:text-gray-400 duration-500 '>{li.name}</Link>
