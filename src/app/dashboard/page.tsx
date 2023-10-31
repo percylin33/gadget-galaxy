7f19cbb30b4a21c80d7c7e71923670f8b2c7fa84
percy <percy.u.lomas@gmail.com>
2023-10-31 01:32:28 -0500

    style-dash-py-4

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,7 +8,7 @@ export default function dashboardPage() {
       
       <div className=' grid  lg:grid-cols-4 xl:grid-cols-6 min-h-screen lg:h-[calc(100vh-4rem)] bg-gris'> 
         <Sidebar />
-        <main className="lg:col-span-3 xl:col-span-5  p-8 py-0">
+        <main className="lg:col-span-3 xl:col-span-5  p-8 py-4">
           <Header />
         </main>
       </div>
