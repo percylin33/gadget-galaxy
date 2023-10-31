203686ce507dbcc6e5ff9af5772cdc450a12e6a6
percy <percy.u.lomas@gmail.com>
2023-10-31 01:18:45 -0500

    fix-header-style

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/Header"
 import Sidebar from "@/components/Sidebar"
 
 import Link from "next/link"
@@ -5,13 +6,10 @@ import Link from "next/link"
 export default function dashboardPage() {
     return (
       
-      <div className=' grid grid-cols-6 min-h-screen lg:h-[calc(100vh-4rem)] bg-rojo'>
-        
+      <div className=' grid  lg:grid-cols-4 xl:grid-cols-6 min-h-screen lg:h-[calc(100vh-4rem)] bg-gris'> 
         <Sidebar />
-        <main className=" bg-rojo">
-          <a href='/'>atras</a>
-          <br/>
-          contenct
+        <main className="lg:col-span-3 xl:col-span-5  p-8 py-0">
+          <Header />
         </main>
       </div>
       
